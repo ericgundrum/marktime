3e3ef7b35dac1b6d699e3c33c7c71b04ea2228ce
Eric Gundrum <git@ericgundrum.com>
2017-03-17 14:36:47 -0700

    Remove unnecessary semicolons using `sed -i '' -e 's/;$//g' *.ts`.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { MarkTimePage } from './app.po';
+import { MarkTimePage } from './app.po'
 
 describe('mark-time App', () => {
-  let page: MarkTimePage;
+  let page: MarkTimePage
 
   beforeEach(() => {
-    page = new MarkTimePage();
-  });
+    page = new MarkTimePage()
+  })
 
   it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
-});
+    page.navigateTo()
+    expect(page.getParagraphText()).toEqual('app works!')
+  })
+})

759722850238a85197646dece7f361b4f6a4867e
Eric Gundrum <git@ericgundrum.com>
2017-04-15 17:42:48 -0700

    Correct the algorithm detecting when the source tree is modified.

--- makeColophone.js
+++ makeColophone.js
@@ -7,7 +7,7 @@ require('fs').writeFileSync( 'src/colophone.js', 'module.exports = ' + JSON.stri
     author: pkg.author,
     build: {
       branch: spawn( 'git', [ 'branch' ] ).stdout.toString().split('* ', 2)[1].split('\n', 1)[0],
-      clean: spawn( 'git', [ 'status' ] ).stdout.toString().length === 0,
+      clean: spawn( 'git', [ 'status' ] ).stdout.toString().endsWith('clean\n'),
       rev: spawn( 'git', [ 'rev-parse', 'HEAD' ] ).stdout.toString().slice(0,8),
     },
     description: pkg.description,

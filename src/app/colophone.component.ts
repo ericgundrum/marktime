abc5e9649e1080fff0453e0358251cb312e20144
Eric Gundrum <git@ericgundrum.com>
2017-04-15 17:30:45 -0700

    Refine and expand colophone presentation with build info...
    Also add image link for more information, using the public domain
    external link image from 'wikimedia'
    
    https://commons.wikimedia.org/wiki/File:External.svg

--- src/app/colophone.component.ts
+++ src/app/colophone.component.ts
@@ -7,10 +7,16 @@ import * as colophone from '../colophone'
     <h1 md-dialog-title>Mark Time</h1>
     <md-dialog-content style="max-width: 29em">
     {{vers.description}}
-    <a target="_blank" href="{{vers.homepage}}">^</a>
+    <a target="_blank" href="{{vers.homepage}}">
+    <img src="/assets/external.svg" width="12" alt="more info">
+    </a>
     <p>
 
     version {{vers.version}}<br/>
+    build
+    {{vers.build.rev}}
+    ({{vers.build.branch}})
+    {{vers.build.clean ? "" : "modified"}}<br/>
     copyright &copy;{{vers.year}} {{vers.author.name}}<br/>
     usage and <a target="_blank" href="{{vers.source}}">source code</a>
     governed by the
